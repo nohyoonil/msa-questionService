6cfa961c587de8ce891ae703dd2e87909cc02e52
yoon <yunil9631@naver.com>
2025-05-17 14:44:24 +0900

    kafka + redis polling으로 변경

--- src/main/java/org/yoon/msaquestionservice/kafka/KafkaConsumer.java
+++ src/main/java/org/yoon/msaquestionservice/kafka/KafkaConsumer.java
@@ -4,11 +4,14 @@ package org.yoon.msaquestionservice.kafka;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import org.yoon.msaquestionservice.Question;
 import org.yoon.msaquestionservice.QuestionRepository;
 import org.yoon.msaquestionservice.model.dto.VoteDetailDto;
+
+import java.time.Duration;
 
 
 @Service
@@ -16,22 +19,17 @@ import org.yoon.msaquestionservice.model.dto.VoteDetailDto;
 public class KafkaConsumer {
 
     private final ObjectMapper objectMapper;
-    private final KafkaProducer kafkaProducer;
+    private final RedisTemplate<String, String> redisTemplate;
     private final QuestionRepository questionRepository;
 
-    @KafkaListener(topics = "validate.question", groupId = "question-service")
+    @KafkaListener(topics = "validate.questionId", groupId = "question-service")
     public void listen(String message) {
         try {
-            VoteDetailDto voteDetail = objectMapper.readValue(message, VoteDetailDto.class);
-            Long questionId = voteDetail.getQuestionId();
-            Question question = questionRepository.findById(questionId)
+            Long questionId = objectMapper.readValue(message, Long.class);
+            questionRepository.findById(questionId)
                     .orElseThrow(() -> new RuntimeException("질문을 찾을 수 없습니다. ID: " + questionId));
 
-            question.plusVotedSum();
-            questionRepository.save(question);
-            voteDetail.setRewards(question.getReward());
-            kafkaProducer.send("validate.member", objectMapper.writeValueAsString(voteDetail));
-
+            redisTemplate.opsForValue().set("questionId:" + questionId, "exist", Duration.ofSeconds(3));
         } catch (JsonProcessingException e) {
             System.err.println("[❌ JSON 파싱 실패] " + e.getMessage());
         } catch (Exception e) {
